759a9c9b51ecfe8d3cf7b85e4eb2925402cd6c6e
AnnaErshova <anna.ershova@gmail.com>
2015-08-25 13:11:16 -0400

    all done

--- largest_product_in_a_series.rb
+++ largest_product_in_a_series.rb
@@ -20,10 +20,14 @@ class LargestProductInASeries
     new_array = []
     i = 0
     number_of_times.times do 
-      new_array << make_into_array_and_convert_contents_to_i[i..i+LENGTH_OF_SAMPLE_SEQUENCE-1]
+      (new_array << make_into_array_and_convert_contents_to_i[i..i+LENGTH_OF_SAMPLE_SEQUENCE-1]) if array_contains_0?(i)
       i +=1
     end
     new_array
+  end
+
+  def array_contains_0?(i)
+    !make_into_array_and_convert_contents_to_i[i..i+LENGTH_OF_SAMPLE_SEQUENCE-1].include?(0)
   end
 
   def number_of_times
